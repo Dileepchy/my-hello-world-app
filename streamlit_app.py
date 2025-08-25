6fc818c40f8590b3d31951934dc204e839f138c6
Dileep Chaudhary <108448416+Dileepchy@users.noreply.github.com>
2025-08-25 16:57:31 +0545

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import streamlit as st
 from numpy.random import default_rng as rng
-import plotly.figure_factory as ff
-from numpy.random import default_rng as rng
+
 
 hist_data = [
     rng(0).standard_normal(200) - 2,
@@ -42,15 +41,4 @@ df = pd.DataFrame(rng(0).standard_normal((20, 3)), columns=["a", "b", "c"])
 
 st.area_chart(df)
 
-hist_data = [
-    rng(0).standard_normal(200) - 2,
-    rng(1).standard_normal(200),
-    rng(2).standard_normal(200) + 2,
-]
-group_labels = ["Group 1", "Group 2", "Group 3"]
 
-fig = ff.create_distplot(
-    hist_data, group_labels, bin_size=[0.1, 0.25, 0.5]
-)
-
-st.plotly_chart(fig)
